5f47eebd4799a90ece2e762bfa668434dfb894e1
Vinay S <vinay.shankar0369@gmail.com>
2025-01-14 01:11:37 +0530

    neon db integration

--- app.py
+++ app.py
@@ -1,17 +1,41 @@
 from flask import Flask, request, jsonify, render_template, make_response
 from flask_cors import CORS
+from dotenv import load_dotenv
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+import os
 
 app = Flask(__name__, template_folder='templates')
 CORS(app)
+load_dotenv()
 
-pages = {}
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
 
 
-@app.route('/<page>', methods=['GET', 'POST'])
+class Page(db.Model):
+    __tablename__ = 'pages'
+
+    id = db.Column(db.Integer, primary_key=True)
+    page_name = db.Column(db.Text, unique=True, nullable=False)
+    content = db.Column(db.Text, nullable=True)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+    updated_at = db.Column(
+        db.DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
+
+
+with app.app_context():
+    db.create_all()
+
+
+@app.route('/<path:page>', methods=['GET', 'POST'])
 def page_handler(page):
     if request.method == 'GET':
-        if page in pages:
-            return make_response(render_template('page.html', title=page, data=pages[page]), 200)
+        page_data = Page.query.filter_by(page_name=page).first()
+        if page_data:
+            return make_response(render_template('page.html', title=page, data=page_data.content), 200)
         else:
             return make_response(render_template('page.html', title=page, data=""), 200)
 
@@ -19,8 +43,16 @@ def page_handler(page):
         data = request.json
         if not data or 'content' not in data:
             return jsonify({'error': 'Invalid data'}), 400
-        pages[page] = data['content']
-        return jsonify({'message': 'Page updated successfully'})
+
+        page_data = Page.query.filter_by(page_name=page).first()
+        if page_data:
+            page_data.content = data['content']
+        else:
+            page_data = Page(page_name=page, content=data['content'])
+            db.session.add(page_data)
+
+        db.session.commit()
+        return jsonify({'message': 'Page updated'}), 200
 
 
 @app.route('/')
